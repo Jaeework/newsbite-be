69e9ad621b245dd6620f6bb6bdbf0b18111abeaf
jieul2 <jieul2@kakao.com>
2026-03-05 23:54:02 +0900

    chore: #6 index.js 코드 가독성 개선

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const newsApi = require("./news.api");
-
-router.use("/news", newsApi);
-
 const authApi = require("./auth.api");
 const userApi = require("./user.api");
 
+router.use("/news", newsApi);
 router.use("/auth", authApi);
 router.use("/me", userApi);
 
