ce0b7c3cde7a9a6c226e05c3a328663a94b477d8
Jaeework <jaeework@gmail.com>
2026-03-05 23:42:26 +0900

    feat: userNews 관련 me route에 auth 미들웨어 적용

--- src/routes/me.api.js
+++ src/routes/me.api.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const userNewsController = require("../controllers/userNews.controller");
+const authMiddleware  = require("../middlewares/auth.middleware");
 
-router.post("/news", userNewsController.createUserNews);
-router.delete("/news/:id", userNewsController.deleteUserNews);
-router.get("/news", userNewsController.getUserNewsList);
-router.post("/news/:id/hide", userNewsController.hideUserNews);
+router.post("/news", authMiddleware, userNewsController.createUserNews);
+router.delete("/news/:id", authMiddleware, userNewsController.deleteUserNews);
+router.get("/news", authMiddleware, userNewsController.getUserNewsList);
+router.post("/news/:id/hide", authMiddleware, userNewsController.hideUserNews);
 
 module.exports = router;
