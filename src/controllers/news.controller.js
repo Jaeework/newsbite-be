269978bc62418fd9dab5c72329598e316b1c171a
Jieul <jieul2@nate.com>
2026-03-09 19:33:42 +0900

    chore: #27 errorapi매개변수 순서 변경, 자동 개행 및 쉼표 스타일 정리

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -35,9 +35,7 @@ newsController.getAllNews = async (req, res, next) => {
       },
     });
 
-    const newsByWord = words.flatMap((w) =>
-      (w.news || []).map((nw) => nw.news)
-    );
+    const newsByWord = words.flatMap((w) => (w.news || []).map((nw) => nw.news));
 
     //중복 제거
     const newsMap = new Map();
@@ -64,9 +62,8 @@ newsController.getAllNews = async (req, res, next) => {
 // 뉴스 상세 조회
 newsController.getNewsById = async (req, res, next) => {
   try {
-    const {userId} = req;
+    const { userId } = req;
     const newsId = req.params.id;
-
 
     // 뉴스 정보 조회
     const news = await News.findById(newsId);
@@ -75,37 +72,37 @@ newsController.getNewsById = async (req, res, next) => {
     }
 
     // 해당 뉴스의 모든 단어 데이터 가져오기
-    const newsWords = await NewsWords.find({ news: newsId }).populate('word');
-    const allWords = newsWords.map(nw => nw.word);
+    const newsWords = await NewsWords.find({ news: newsId }).populate("word");
+    const allWords = newsWords.map((nw) => nw.word);
 
     // 약어(abbreviation)와 일반 단어 분리
-    const abbreviations = allWords.filter(word => word.type === 'abbreviation');
-    const baseWords = allWords.filter(word => word.type !== 'abbreviation');
+    const abbreviations = allWords.filter((word) => word.type === "abbreviation");
+    const baseWords = allWords.filter((word) => word.type !== "abbreviation");
 
     // 일반 단어 처리 (유저 존재 여부에 따른 분기)
     let words = [];
 
     if (userId) {
       // 로그인이 된 경우: DB에서 사용자의 학습 현황 조회
-      const wordIds = baseWords.map(word => word._id);
+      const wordIds = baseWords.map((word) => word._id);
       const userWords = await UserWord.find({
         user: userId,
-        word: { $in: wordIds }
+        word: { $in: wordIds },
       });
 
       const userWordStatusMap = {};
-      userWords.forEach(uw => {
+      userWords.forEach((uw) => {
         userWordStatusMap[uw.word.toString()] = uw.isDone;
       });
 
-      words = baseWords.map(word => {
+      words = baseWords.map((word) => {
         const wordObj = word.toObject();
         wordObj.isDone = userWordStatusMap[word._id.toString()] || false;
         return wordObj;
       });
     } else {
       // 유저 정보가 없는 경우: 모든 단어를 false로 설정
-      words = baseWords.map(word => {
+      words = baseWords.map((word) => {
         const wordObj = word.toObject();
         wordObj.isDone = false;
         return wordObj;
@@ -118,8 +115,8 @@ newsController.getNewsById = async (req, res, next) => {
       data: {
         news,
         words,
-        abbreviations
-      }
+        abbreviations,
+      },
     });
   } catch (err) {
     next(err);
