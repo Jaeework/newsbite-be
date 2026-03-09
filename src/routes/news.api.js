8d5096ebb79a6a7d200c6eef90dd337e9acfef69
Jieul <jieul2@nate.com>
2026-03-10 00:07:09 +0900

    chore: #27 미미들웨어 이름 변경

--- src/routes/news.api.js
+++ src/routes/news.api.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const newsController = require("../controllers/news.controller");
-const optionalAuth = require("../middlewares/news.auth.middleware");
+const optionalAuth = require("../middlewares/option.auth.middleware");
 
 // 뉴스 목록 조회 (전체 조회 및 단어 검색 검색 통합)
 // 예시: GET /news  (전체 조회)
